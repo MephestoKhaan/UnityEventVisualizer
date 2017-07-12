c29d1644cdc0c0b1d57e1005dff7af6c78f150cf
ANALOG\lmefisto <luca.m.conesa@gmail.com>
2017-07-12 16:53:32 +0100

    initial commit with Event detection

---
+++ Assets/_Scripts/EventsFinder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class EventsFinder : MonoBehaviour
+{
+
+    void Start()
+    {
+
+        Component[] allComponents = ScriptableObject.FindObjectsOfType<Component>();
+
+        foreach (Component component in allComponents)
+        {
+            SerializedObject serializedObject = new UnityEditor.SerializedObject(component);
+            SerializedProperty iterator = serializedObject.GetIterator();
+            iterator.Next(true);
+
+            while (iterator.Next(false))
+            {
+                if (iterator.type != null
+                    && iterator.type != string.Empty)
+                {
+                    SerializedProperty persistentCalls = iterator.FindPropertyRelative("m_PersistentCalls.m_Calls");
+                    if (persistentCalls != null)
+                    {
+
+                        for (int i = 0; i < persistentCalls.arraySize; ++i)
+                        {
+                            SerializedProperty methodName = persistentCalls.GetArrayElementAtIndex(i).FindPropertyRelative("m_MethodName");
+                            SerializedProperty target = persistentCalls.GetArrayElementAtIndex(i).FindPropertyRelative("m_Target");
+
+                            Debug.Log("InstanceID: " + target.objectReferenceInstanceIDValue + " calls: " + methodName.stringValue);
+                            //callState.intValue = (int)UnityEngine.Events.UnityEventCallState.RuntimeOnly;
+                        }
+                    }
+
+                }
+            }
+
+        }
+    }
+
+
+}
