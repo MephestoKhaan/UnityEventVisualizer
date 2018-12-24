b18774ab269b103df7f13a0d49bf5fad3fd78939
Andrés <forestrf@gmail.com>
2018-12-25 00:15:59 +0100

    Removed duplicated code. Set default zoom to 1 (Can't read text at the default 0.5f)

--- Assets/EventVisualizer/Editor/EventsGraphWindow.cs
+++ Assets/EventVisualizer/Editor/EventsGraphWindow.cs
@@ -14,7 +14,7 @@ namespace EventVisualizer.Base
 		private const float kZoomMax = 1.0f;
 
 		private Rect _zoomArea = new Rect(0.0f, 75.0f, 600.0f, 300.0f - 100.0f);
-		private float _zoom = 0.5f;
+		private float _zoom = 1f;
 		private Vector2 _zoomCoordsOrigin = Vector2.zero;
 
 		private const float kBarHeight = 17;
@@ -130,76 +130,5 @@ namespace EventVisualizer.Base
 				_graph.RefreshGraphConnections();
 			}
 		}
-	}
-}
-
-
-
-public static class RectExtensions
-{
-	public static Vector2 TopLeft(this Rect rect)
-	{
-		return new Vector2(rect.xMin, rect.yMin);
-	}
-	public static Rect ScaleSizeBy(this Rect rect, float scale)
-	{
-		return rect.ScaleSizeBy(scale, rect.center);
-	}
-	public static Rect ScaleSizeBy(this Rect rect, float scale, Vector2 pivotPoint)
-	{
-		Rect result = rect;
-		result.x -= pivotPoint.x;
-		result.y -= pivotPoint.y;
-		result.xMin *= scale;
-		result.xMax *= scale;
-		result.yMin *= scale;
-		result.yMax *= scale;
-		result.x += pivotPoint.x;
-		result.y += pivotPoint.y;
-		return result;
-	}
-	public static Rect ScaleSizeBy(this Rect rect, Vector2 scale)
-	{
-		return rect.ScaleSizeBy(scale, rect.center);
-	}
-	public static Rect ScaleSizeBy(this Rect rect, Vector2 scale, Vector2 pivotPoint)
-	{
-		Rect result = rect;
-		result.x -= pivotPoint.x;
-		result.y -= pivotPoint.y;
-		result.xMin *= scale.x;
-		result.xMax *= scale.x;
-		result.yMin *= scale.y;
-		result.yMax *= scale.y;
-		result.x += pivotPoint.x;
-		result.y += pivotPoint.y;
-		return result;
-	}
-}
-
-public class EditorZoomArea
-{
-	private static Matrix4x4 _prevGuiMatrix;
-
-	public static Rect Begin(float zoomScale, Rect screenCoordsArea)
-	{
-		GUI.EndGroup();
-
-		Rect clippedArea = screenCoordsArea.ScaleSizeBy(1.0f / zoomScale, screenCoordsArea.TopLeft());
-		GUI.BeginGroup(clippedArea);
-
-		_prevGuiMatrix = GUI.matrix;
-		Matrix4x4 translation = Matrix4x4.TRS(clippedArea.TopLeft(), Quaternion.identity, Vector3.one);
-		Matrix4x4 scale = Matrix4x4.Scale(new Vector3(zoomScale, zoomScale, 1.0f));
-		GUI.matrix = translation * scale * translation.inverse * GUI.matrix;
-
-		return clippedArea;
-	}
-
-	public static void End()
-	{
-		GUI.matrix = _prevGuiMatrix;
-		GUI.EndGroup();
-		GUI.BeginGroup(new Rect(0.0f, 0.0f, Screen.width, Screen.height));
 	}
 }
